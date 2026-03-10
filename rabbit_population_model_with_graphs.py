ab23f9727893f7e9cde95a13d1f515cf1112b874
Me <me@personal.com>
2026-03-10 09:23:47 -0400

    Skeleton

---
+++ rabbit_population_model_with_graphs.py
@@ -0,0 +1,12 @@
+#Model the number of rabbits from a starting population and each successive generation
+#population model: x_(n+1) = rx_n(1-x_n)
+#x=rabbit population
+#r=growth rate
+#n=year
+
+#Find out how to communicate the population model to python
+#Find out how to put graphs in python
+#Prompt the user for growth rate and initial population
+#Graph the population model starting with a specific population and growth rate
+
+#Figure out what the second graph was supposed to be because you didn't take good enough notes
