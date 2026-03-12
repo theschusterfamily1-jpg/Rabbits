d643d844c13bb0d1a2ee4258916ee6fea5ea5e0a
Me <me@personal.com>
2026-03-12 09:48:57 -0400

    Working sliders, one chart

--- rabbit_population_model_with_graphs.py
+++ rabbit_population_model_with_graphs.py
@@ -7,31 +7,71 @@
 #Find out how to put graphs in python
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.widgets import Button, Slider
 
-initial_pop = .4
+initial_pop = .3
+growth_rate = 2.6
 
-x = []
-y = []
 #x_1 = r*x*(1-x)
 
-for i in range(100):
-    x.append(i)
-    y.append(2.6*initial_pop*(1-initial_pop))
-    initial_pop = y[-1]
+def set_points(r,p):
+    x=[]
+    y=[]
+    for i in range(100):
+        x.append(i)
+        y.append(r*p*(1-p))
+        p = y[-1]
+    return x, y
+
+init_x, init_y = set_points(growth_rate, initial_pop)
 
 fig, ax = plt.subplots()
-ax.plot(x,y)
+ax.plot(init_x,init_y)
+fig.subplots_adjust(bottom=0.35)
+
+#Growth Rate Slider
+axgrow = fig.add_axes([0.25, .2, 0.65, 0.03])
+grow_slider = Slider(
+    ax=axgrow,
+    label ='Growth rate',
+    valmin=0,
+    valmax=4,
+    valinit=growth_rate
+)
+
+#Initial Population Slider
+axpop = fig.add_axes([0.25, 0.1, 0.65, 0.03])
+pop_slider = Slider(
+    ax=axpop,
+    label = 'Initial Population',
+    valmin = 0,
+    valmax = 1,
+    valinit = initial_pop
+)
+
+# The function to be called anytime a slider's value changes
+
+def update_graph(val):
+    growth_rate = grow_slider.val
+    initial_pop = pop_slider.val
+    ax.clear()
+    x, y = set_points(growth_rate, initial_pop)
+    ax.plot(x,y)
+
+
+grow_slider.on_changed(update_graph)
+pop_slider.on_changed(update_graph)
+
+
+
+#Graph Labels
 ax.set_xlabel('Time')
 ax.set_ylabel('Population')
 ax.set_title("Population Over Time")
 
 plt.show()
 
-#Find out how to communicate the population model to python
-#y=rx(1-x)
-#set y to a variable that feeds back into the equation as the next value of x?
 
-#Prompt the user for initial population (fraction of 1)
 #Graph the population model starting with a specific population and growth rate of 0, rising to 4
 
 #the two graphs should be:
